ce0ce323f60abd304eb2f2e4a123caedb18c9eb0
Chanwoo Jeong <enghqii@gmail.com>
2014-05-14 14:36:48 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		frame.setSize(300, 400);
+		frame.setTitle("JFrame Test");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
